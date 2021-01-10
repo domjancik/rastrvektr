5d4ff87cc66e167a9ba93132ebdfaddf182c124e
Dominik Jancik <hi@domj.net>
2021-01-10 09:09:42 +0200

    Automatic menu from files in src/apps

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { apps } from 'app-info';
 
 const initApp = ( { init } ) => {
     document.body.innerHTML = '';
@@ -8,12 +9,38 @@ const loadApp = async ( name ) => {
     initApp(await import(`./apps/${name}`))
 }
 
+window.loadApp = loadApp;
 
 document.body.innerText = 'Menu here maybe';
+
+const makeMenuItem = appName => {
+    const button = document.createElement('button');
+    button.innerText = appName;
+    button.onclick = () => loadApp(appName);
+
+    const li = document.createElement('li');
+    li.appendChild(button);
+
+    return li;
+}
+
+const makeMenu = appNames => {
+    const ul = document.createElement('ul');
+    
+    appNames.forEach(appName => {
+        ul.appendChild(makeMenuItem(appName));
+    });
+
+    return ul;
+}
+
+console.log(apps);
+document.body.appendChild(makeMenu(apps));
+
 
 // setInterval(loadSquarium, 2000);
 
 // loadApp('squarium');
-loadApp('squarium_with_speech');
+// loadApp('squarium_with_speech');
 // loadApp('speech_to_text');
 // loadApp('speech_to_text_continuous');
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,17 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const VirtualModulesPlugin = require("webpack-virtual-modules");
+
+const fs = require("fs");
+
+const getApps = () => {
+    return fs.readdirSync('./src/apps').map(appName => appName.replace(/\.js$/, ''));
+}
+
+const virtualModules = new VirtualModulesPlugin({
+    'node_modules/app-info.js': `exports.apps = ${JSON.stringify(getApps())};`
+})
 
 module.exports = {
     devtool: 'source-map',
-    plugins: [new HtmlWebpackPlugin()]
+    plugins: [new HtmlWebpackPlugin(), virtualModules]
 }
