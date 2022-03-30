5cc3f60680531d3b937e05ec29a02fee4e6c2992
Dominik Jancik <hi@domj.net>
2022-03-30 19:48:45 +0300

    it's about time these old files be pushed

--- src/apps/squarium_with_speech.js
+++ src/apps/squarium_with_speech.js
@@ -1,8 +1,12 @@
 import flyd from 'flyd';
+import { lowerCase } from 'lodash';
 import { listen } from '../io/in/speech_input_continuous';
+import { CSS_COLOR_NAMES } from '../utils/css_colors';
 import { init as squariumInit, colors } from './squarium';
 
 const validColors = ['magenta', 'yellow', 'green', 'blue'];
+
+const validColorPhrases = CSS_COLOR_NAMES.map(lowerCase);
 
 const init = () => {
     squariumInit();
@@ -11,10 +15,12 @@ const init = () => {
     
     flyd.combine(resultStream => {
         const result = resultStream();
-        const color = validColors.find(color => result.transcript.includes(color));
+        const color = validColorPhrases.find(color => result.transcript.includes(color));
+        const cssColor = color && color.replace(' ', '');
 
         console.log(color);
-        if (color) colors.push(color);
+        console.log(cssColor);
+        if (color) colors.push(cssColor);
     }, [listen()]);
 }
 
